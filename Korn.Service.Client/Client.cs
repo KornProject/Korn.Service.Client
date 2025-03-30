c9e082b70a9a2051f6b021022768d750262a4886
Yotic <yoticcitoy@gmail.com>
2025-03-30 07:42:18 +0300

    A working concept

---
+++ Korn.Service.Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using Korn.Pipes;
+using System;
+
+namespace Korn.Service
+{
+    public class Client
+    {
+        public Client(ServerConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            ConnectionID = new ConnectionID(configuration);
+
+            ConnectPipe = new InputPipe(configuration.ConnectConfiguration)
+            {
+                Connected = OnConnectPipeConnected
+            };
+
+            Connection = new ServerConnection(ConnectionID)
+            {
+                Received = OnReceived,
+            };
+        }
+
+        public readonly ServerConfiguration Configuration;
+        public readonly ConnectionID ConnectionID;
+        public readonly ServerConnection Connection;
+        public readonly InputPipe ConnectPipe;
+
+        public Action<ServerPacket, uint> Received;
+
+        void OnConnectPipeConnected()
+        {
+            var bytes = BitConverter.GetBytes(ConnectionID.Indentitifer);
+            ConnectPipe.Send(bytes);
+        }
+
+        void OnReceived(byte[] bytes)
+        {
+            PacketSerializer.DeserializeServerPacket(Configuration, bytes, out var packet, out var id);
+
+            Received?.Invoke(packet, id);
+        }
+
+        public void Send(ClientPacket packet)
+        {
+            var bytes = PacketSerializer.SerializeClientPacket(Configuration, packet);
+            Connection.Send(bytes);
+        }
+    }
+}
---
+++ Korn.Service.Client/ServerConnection.cs
@@ -0,0 +1,34 @@
+﻿using Korn.Pipes;
+using System;
+
+namespace Korn.Service
+{
+    public class ServerConnection : Connection
+    {
+        public ServerConnection(ConnectionID identifier) : base(identifier)
+        {
+            ConnectionID = identifier;
+
+            InputPipe = new InputPipe(identifier.InServerConfiguration)
+            {
+                Connected = OnPipeConnected,
+                Disconnected = OnPipeDisconnected,
+            };
+
+            OutputPipe = new OutputPipe(identifier.InClientConfiguration)
+            {
+                Received = bytes =>
+                {
+                    if (Received != null)
+                        Received.Invoke(bytes);
+                },
+                Connected = OnPipeConnected,
+                Disconnected = OnPipeDisconnected,
+            };
+        }
+
+        public readonly ConnectionID ConnectionID;
+
+        public Action<byte[]> Received;
+    }
+}
